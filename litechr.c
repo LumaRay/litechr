2ff16e3e0e389105b8fa40eba3e7cffa775f891f
Yury Laykov <sowulo@inbox.ru>
2023-01-24 10:55:00 +0300

    Improved version with read/write support

--- litechr.c
+++ litechr.c
@@ -1,12 +1,16 @@
 #define pr_fmt(fmt) "%s:%s: " fmt, KBUILD_MODNAME, __func__
 
+#include <linux/uaccess.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/slab.h>
 #include <linux/cdev.h>
 #include <linux/fs.h>
 
 //Device details 
 #define DEVICE_NAME "litechr"
+
+#define MAX_BUF_SIZE 1000
 
 static dev_t litechr_dev;
 static struct cdev litechr_cdev;
@@ -14,8 +18,8 @@ static struct class *plitechr_class;
 
 static int litechr_open(struct inode *pinode, struct file *pfile);
 static int litechr_release(struct inode *pinode, struct file *pfile);
-static ssize_t litechr_read(struct file *pfile, char *buffer, size_t length, loff_t *poffset);
-static ssize_t litechr_write(struct file *pfile, const char *buffer, size_t length, loff_t *poffset);
+static ssize_t litechr_read(struct file *pfile, char *ubuf, size_t length, loff_t *poffset);
+static ssize_t litechr_write(struct file *pfile, const char *ubuf, size_t length, loff_t *poffset);
 
 static int litechr_uevent(struct device *pdev, struct kobj_uevent_env *penv);
 
@@ -26,6 +30,16 @@ static struct file_operations litechr_fops = {
     .read = litechr_read,
     .write = litechr_write,
 };
+
+struct data_queue_entry_t {
+    struct list_head entry_head;
+    uint8_t data_byte;
+};
+
+static LIST_HEAD(data_queue_head);
+static struct list_head *pdata_queue_tail = &data_queue_head;
+
+//static struct data_queue_entry_t data_queue;
 
 // Initialize the driver
 static int __init litechr_init(void)
@@ -88,6 +102,15 @@ un_reg:
 // Deinitialize driver
 static void __exit litechr_exit(void)
 {
+    struct data_queue_entry_t *pentry, *ptentry;
+    size_t count = 0;
+    list_for_each_entry_safe(pentry, ptentry, &data_queue_head, entry_head) {
+        list_del(&pentry->entry_head);
+        kfree(pentry);
+	count++;
+    }
+    pr_info("Cleared %ld entries\n", count);
+
     device_destroy(plitechr_class, litechr_dev);
  
     // Delete character device
@@ -117,18 +140,90 @@ static int litechr_release(struct inode *pinode, struct file *pfile)
     return 0;
 }
 
-static ssize_t litechr_read(struct file *pfile, char *buffer, size_t length, loff_t *poffset)
+static ssize_t litechr_read(struct file *pfile, char *ubuf, size_t length, loff_t *poffset)
 {
-    //TODO: Add your code here
- 
-    return 0;
+    size_t buf_len;
+    char *kbuf;
+    struct data_queue_entry_t *pentry;
+
+    if (*poffset != 0)
+        return -ESPIPE;
+    if (length == 0)
+        return -EINVAL;
+    if (ubuf == NULL)
+        return -EINVAL;
+    
+    kbuf = kmalloc(MAX_BUF_SIZE, GFP_KERNEL);
+    if (kbuf == NULL)
+        return -ENOMEM;
+
+    pr_info("Sending data: ");
+
+    for (   buf_len = 0;
+            buf_len < length && !list_empty(pdata_queue_tail);
+            ++buf_len ) {
+        pentry = list_entry(pdata_queue_tail, struct data_queue_entry_t, entry_head);
+        kbuf[buf_len] = pentry->data_byte;
+        pdata_queue_tail = pdata_queue_tail->next;
+        list_del(&pentry->entry_head);
+        kfree(pentry);
+        pr_cont("%c (0x%02X) ", kbuf[buf_len], kbuf[buf_len]);
+    }
+    pr_info("");
+
+    if (copy_to_user(ubuf, kbuf, buf_len)) {
+        kfree(kbuf);
+        return -EFAULT;
+    }
+
+    kfree(kbuf);
+
+    return buf_len;
 }
 
-static ssize_t litechr_write(struct file *pfile, const char *buffer, size_t length, loff_t *poffset)
+static ssize_t litechr_write(struct file *pfile, const char *ubuf, size_t length, loff_t *poffset)
 {
-    //TODO: Add your code here
- 
-    return 0;
+    uint8_t *kbuf;
+    int i;
+    struct data_queue_entry_t *pnew_entry;
+    
+    if (*poffset != 0)
+        return -ESPIPE;
+    if (length == 0)
+        return -EINVAL;
+    if (length > MAX_BUF_SIZE)
+        return -ENOBUFS;
+    if (ubuf == NULL)
+        return -EINVAL;
+
+    kbuf = kmalloc(MAX_BUF_SIZE, GFP_KERNEL);
+    if (kbuf == NULL)
+        return -ENOMEM;
+
+    if (copy_from_user(kbuf, ubuf, length)) {
+        kfree(kbuf);
+        return -EFAULT;
+    }
+
+    pr_info("Incoming data: ");
+    for (i = 0; i < length; ++i) {
+        pnew_entry = kzalloc(sizeof(struct data_queue_entry_t), GFP_KERNEL);
+        if (pnew_entry == NULL) {
+            kfree(kbuf);		
+            return -ENOMEM;
+        }
+        pnew_entry->data_byte = kbuf[i];
+	INIT_LIST_HEAD(&pnew_entry->entry_head);
+	if (list_empty(&data_queue_head))
+            pdata_queue_tail = &pnew_entry->entry_head;
+	list_add_tail(&pnew_entry->entry_head, &data_queue_head);
+	pr_cont("%c (0x%02X) ", kbuf[i], kbuf[i]);
+    }
+    pr_info("");
+
+    kfree(kbuf);
+
+    return length;
 }
 
 static int litechr_uevent(struct device *pdev, struct kobj_uevent_env *penv)
